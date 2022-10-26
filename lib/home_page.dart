8e0c80547ff0f74d14aeade3bdf6f74586ad4ab3
yeonjoo.lee <teddyjune@naver.com>
2022-10-26 14:24:03 +0900

    feat: 구글 로그아웃 구현중

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final googleSignIn = GoogleSignIn();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -12,6 +15,13 @@ class HomePage extends StatelessWidget {
           'Instagram Clone',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                logout();
+              },
+              icon: const Icon(Icons.logout)),
+        ],
       ),
       body: _buildbody(),
     );
@@ -92,4 +102,10 @@ class HomePage extends StatelessWidget {
       ),
     ));
   }
+}
+
+void logout() async {
+  await googleSignIn.signOut();
+  await FirebaseAuth.instance.signOut();
+  // google signOut
 }
